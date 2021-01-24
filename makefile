f1c150ea991e6f5ffb2e12ab4cf7e72000aa02f8
TR07SKY <zaffranivalentino@gmail.com>
2021-01-24 13:05:51 +0000

    general cleanup

--- makefile
+++ makefile
@@ -1,22 +1,18 @@
-OBJS	= utils/util.o crypto/scrypt.o test.o
-SOURCE	= utils/util.cpp crypto/scrypt.c test.cpp
+OBJS	= crypto/scrypt.o test.o
+SOURCE	= crypto/scrypt.c test.c
 HEADER	=
 OUT	    = test.out
-CC	    = g++
 FLAGS	= -g -c -Wall
 LFLAGS	=
 
 all: $(OBJS)
 	$(CC) -g $(OBJS) -o $(OUT) $(LFLAGS)
 
-utils/util.o: utils/util.cpp
-	$(CC) $(FLAGS) utils/util.cpp -o utils/util.o
-
 crypto/scrypt.o: crypto/scrypt.c
 	gcc $(FLAGS) crypto/scrypt.c -o crypto/scrypt.o
 
-test.o: test.cpp
-	$(CC) $(FLAGS) test.cpp -o test.o
+test.o: test.c
+	g++ $(FLAGS) test.c -o test.o
 
 clean:
 	rm -f $(OBJS) $(OUT)
--- test.c
+++ test.c
@@ -1,35 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <inttypes.h>
+#include <time.h>
+#include <math.h>
 
 #include "crypto/scrypt.h"
-#include "utils/util.h"
+#include "utils/uint256.h"
 
-#define STR_LEN 160
+#define BEGIN(a)            ((char*)&(a))
+#define END(a)              ((char*)&((&(a))[1]))
+
+#define HEX_STR_LEN 160
 
 int main(void)
 {
-    unsigned char str[STR_LEN + 1] = {0};
+    unsigned char str[HEX_STR_LEN + 1] = {0};
     const char* hex_digits = "0123456789abcdef";
 
     srand(time(NULL));
 
-    for(int i = 0 ; i < STR_LEN ; i++ )
+    for(int i = 0 ; i < HEX_STR_LEN ; i++ )
         str[i] = hex_digits[(rand() % 16)];
 
     uint256 scrypthash;
 
-    const uint32_t N = 1024;
-    const uint32_t r = 2048;
+    const uint32_t N = 128;
+    const uint32_t r = 9216; //9216
     const uint32_t p = 1;
 
     char* sp_size = (char*)malloc(sizeof(char)*(63 + (128 * r * p) + (256 * r + 64) + (128 * r * N)));
-    //unsigned char sp_size[63 + (128 * r * p) + (256 * r + 64) + (128 * r * N)];
 
     printf("Random hex: %s\n", str);
     printf("N: %u\n", N);
     printf("r: %u\n", r);
     printf("p: %u\n", p);
-    printf("Scratchpad size: %lu\n", 63 + (128 * r * p) + (256 * r + 64) + (128 * r * N));
+    printf("Scratchpad size: %lu\n", (63 + (128 * r * p) + (256 * r + 64) + (128 * r * N)));
     printf("Memory usage: %u MB\n", ((128 * N * r * p) / 1024) / 1024);
 
     scrypt_custom_256((const char*)&str[0], BEGIN(scrypthash), sp_size, N, r, p);
