d6e09dccab2745d19e2546c82b5661142cc9686f
TR07SKY <zaffranivalentino@gmail.com>
2021-01-24 09:28:16 +0000

    Add a random hex generator and more parameters

--- test.cpp
+++ test.cpp
@@ -4,20 +4,30 @@
 #include "crypto/scrypt.h"
 #include "utils/util.h"
 
+#define STR_LEN 160
+
 int main(void)
 {
-    const char* inputhex = "020000004c1271c211717198227392b029a64a7971931d351b387bb80db027f270411e398a07046f7d4a08dd815412a8712f874a7ebf0507e3878bd24e20a3b73fd750a667d2f451eac7471b00de6659";
-    std::vector<unsigned char> inputbytes = ParseHex(inputhex);
+    unsigned char str[STR_LEN + 1] = {0};
+    const char* hex_digits = "0123456789abcdef";
+
+    srand(time(NULL));
+
+    for(int i = 0 ; i < STR_LEN ; i++ )
+        str[i] = hex_digits[(rand() % 16)];
 
     uint256 scrypthash;
 
     const uint32_t N = 1024;
-    const uint32_t r = 1;
+    const uint32_t r = 32;
     const uint32_t p = 1;
 
     char sp_size[63 + (128 * r * p) + (256 * r + 64) + (128 * r * N)];
 
-    scrypt_custom_256((const char*)&inputbytes[0], BEGIN(scrypthash), sp_size, N, r, p);
+    printf("Random hex: %s\n", str);
+    printf("Memory usage: %u MB\n", ((128 * N * r * p) / 1024) / 1024);
+
+    scrypt_custom_256((const char*)&str[0], BEGIN(scrypthash), sp_size, N, r, p);
 
     printf("%lx\n", scrypthash);
 
